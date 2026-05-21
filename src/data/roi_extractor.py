c2d03062cc5003b9f6139a68d0b2334e50e5a5eb
RFLB <144320840+Rackhel@users.noreply.github.com>
2026-05-21 14:03:15 +0900

    Create empty or minimal stub modules so imports don't fail

---
+++ src/data/roi_extractor.py
@@ -0,0 +1,8 @@
+"""ROI extraction utilities (stub)."""
+
+class ROIExtractor:
+    def __init__(self, cfg):
+        self.cfg = cfg
+
+    def extract_rois(self, image, bboxes):
+        raise NotImplementedError
---
+++ src/inference/onnx_export.py
@@ -0,0 +1,5 @@
+"""ONNX export utilities (stub)."""
+
+def export_to_onnx(checkpoint_path, output_path, cfg):
+    """Export model to ONNX format."""
+    raise NotImplementedError("export_to_onnx not yet implemented")
